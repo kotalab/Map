d17040d06818f088b1c50b72543577a19ab793db
kotalab <kotarolab@gmail.com>
2019-05-05 21:41:24 +0900

    change main view to UITableViewController

--- Map/LocationManager.swift
+++ Map/LocationManager.swift
@@ -7,17 +7,31 @@
 //
 
 import CoreLocation
+import GoogleMaps
 
-final class LocationManager {
+final class LocationManager: NSObject {
     static let shared: LocationManager = LocationManager()
     private let clLocationManager: CLLocationManager
 
-    private init () {
+    var didUpdateLocation: ((CLLocation) -> Void)?
+    var didChangeAuthorization: ((String) -> Void)?
+
+    private override init () {
         clLocationManager = CLLocationManager()
+        clLocationManager.desiredAccuracy = kCLLocationAccuracyBest
+        clLocationManager.distanceFilter = 50
+        clLocationManager.startUpdatingLocation()
+
+        super.init()
+        clLocationManager.delegate = self
     }
 
     func requestAuthorization() {
         clLocationManager.requestWhenInUseAuthorization()
+    }
+
+    var authorizationStatus: String {
+        return CLLocationManager.authorizationStatus().string
     }
 
     var canUseLocation: Bool {
@@ -27,4 +41,31 @@ final class LocationManager {
     func currentLocation() -> CLLocation {
         return clLocationManager.location!
     }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        didChangeAuthorization?(status.string)
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+
+        didUpdateLocation?(location)
+    }
+}
+
+private extension CLAuthorizationStatus {
+    var string: String {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways: return "authorizedAlways"
+        case .authorizedWhenInUse: return "authorizedWhenInUse"
+        case .denied: return "denied"
+        case .notDetermined: return "notDetermined"
+        case .restricted: return "restricted"
+        @unknown default:
+            fatalError()
+        }
+    }
+
 }
---
+++ Map/MainTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MainTableViewController.swift
+//  Map
+//
+//  Created by 小島徹也 on 2019/05/05.
+//  Copyright © 2019 kotalab.com. All rights reserved.
+//
+
+import UIKit
+
+final class MainTableViewController: UITableViewController {
+
+    @IBOutlet private weak var statusLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        statusLabel.text = LocationManager.shared.authorizationStatus
+        LocationManager.shared.didChangeAuthorization = { [weak self] status in
+            self?.statusLabel.text = status
+        }
+        tableView.tableFooterView = UIView()
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 && indexPath.row == 1 {
+            LocationManager.shared.requestAuthorization()
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+
+        if
+            indexPath.section == 1 &&
+                indexPath.row == 0 &&
+                LocationManager.shared.canUseLocation
+        {
+            let currentLocation = LocationManager.shared.currentLocation()
+            let mapViewController = MapViewController(location: currentLocation.coordinate)
+            navigationController?.pushViewController(mapViewController, animated: true)
+        }
+    }
+}
